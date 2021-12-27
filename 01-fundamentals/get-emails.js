112cd63ab2f483621a325f0aad2f9d5c2b4fdfce
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-12-27 22:23:39 +0800

    allow additional arguments passed to script

--- 01-fundamentals/get-emails.js
+++ 01-fundamentals/get-emails.js
@@ -2,6 +2,11 @@ const fs = require('fs/promises');
 const path = require('path');
 
 (async function getEmails() {
+  const args = process.argv.slice(2);
+
+  const gender = args[0] || 'F';
+  const outFileName = args[1] || 'female-emails.txt';
+
   try {
     const input = path.resolve(__dirname, 'data.csv');
 
@@ -13,7 +18,7 @@ const path = require('path');
 
     rows.forEach((row, index) => {
       if (index !== 0) {
-        if (row.includes(',F,')) {
+        if (row.includes(`,${gender},`)) {
           const email = row.split(',').pop();
 
           emails.push(email);
@@ -21,7 +26,7 @@ const path = require('path');
       }
     });
 
-    const outputPath = path.resolve(__dirname, 'female-emails.txt');
+    const outputPath = path.resolve(__dirname, outFileName);
 
     await fs.writeFile(outputPath, emails.join('\n'), 'utf-8');
     console.log(`Done collecting ${emails.length} emails.`);
