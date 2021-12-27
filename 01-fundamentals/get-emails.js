a0bea9343949a7bff03c5f55cf43aa77082d3df8
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-12-27 22:20:10 +0800

    use path to resolve the file from the script

--- 01-fundamentals/get-emails.js
+++ 01-fundamentals/get-emails.js
@@ -1,8 +1,11 @@
 const fs = require('fs/promises');
+const path = require('path');
 
 (async function getEmails() {
   try {
-    const data = await fs.readFile('data.csv', 'utf-8');
+    const input = path.resolve(__dirname, 'data.csv');
+
+    const data = await fs.readFile(input, 'utf-8');
 
     const rows = data.split('\n');
 
@@ -18,7 +21,9 @@ const fs = require('fs/promises');
       }
     });
 
-    await fs.writeFile('female-emails.txt', emails.join('\n'), 'utf-8');
+    const outputPath = path.resolve(__dirname, 'female-emails.txt');
+
+    await fs.writeFile(outputPath, emails.join('\n'), 'utf-8');
     console.log(`Done collecting ${emails.length} emails.`);
   } catch (err) {
     console.error(err);
