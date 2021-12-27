98904462326987d7947f7a0fd018132eb2644d67
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-12-28 00:06:46 +0800

    move readCsv function to its own module

--- 01-fundamentals/get-emails.js
+++ 01-fundamentals/get-emails.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises');
 const path = require('path');
+const readCsv = require('./read-csv');
 
 (async function getEmails() {
   const args = process.argv.slice(2);
@@ -30,10 +31,3 @@ const path = require('path');
     console.error(err);
   }
 })();
-
-async function readCsv(filePath, hasHeader = true) {
-  const data = await fs.readFile(filePath, 'utf-8');
-  const rows = data.split('\n');
-
-  return hasHeader ? rows.slice(1) : rows;
-}
---
+++ 01-fundamentals/read-csv.js
@@ -0,0 +1,10 @@
+const fs = require('fs/promises');
+
+async function readCsv(filePath, hasHeader = true) {
+  const data = await fs.readFile(filePath, 'utf-8');
+  const rows = data.split('\n');
+
+  return hasHeader ? rows.slice(1) : rows;
+}
+
+module.exports = readCsv;
