231025d30db940f8085ac0f3364b1e1ed0f5c3ad
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-12-29 09:50:26 +0800

    use csv-parser in getEmailsByProvider

--- 01-fundamentals/get-emails-by-provider.js
+++ 01-fundamentals/get-emails-by-provider.js
@@ -1,5 +1,18 @@
 const fs = require('fs/promises');
 const path = require('path');
+const { parse } = require('csv-parse');
+
+function parseCsv(raw) {
+  return new Promise((fulfill, reject) => {
+    parse(raw, { columns: true }, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        fulfill(rows);
+      }
+    });
+  });
+}
 
 async function getEmailsByProvider(provider) {
   if (provider !== 'gmail' && provider !== 'hotmail' && provider !== 'yahoo') {
@@ -12,17 +25,13 @@ async function getEmailsByProvider(provider) {
 
     const data = await fs.readFile(dataFilePath, 'utf-8');
 
-    const lines = data.split('\n');
+    const lines = await parseCsv(data);
 
     const emails = [];
 
-    lines.forEach((line, index) => {
-      if (index !== 0) {
-        const email = line.split(',').pop();
-
-        if (email.endsWith(`@${provider}.com`)) {
-          emails.push(email);
-        }
+    lines.forEach((line) => {
+      if (line.Email.endsWith(`@${provider}.com`)) {
+        emails.push(line.Email);
       }
     });
 
