d39d04d42cdd955c54a0468c51b8fbe3d6d47e3f
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-01-01 15:49:52 +0800

    Use express to recreate basic server

---
+++ 03-server/basic-express-server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.end('Hello World');
+});
+
+app.get('/end', (req, res) => {
+  res.end('Bye');
+});
+
+app.post('/end', (req, res) => {
+  res.json({
+    message: 'bye',
+  });
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send(`Not Found`);
+});
+
+app.listen(7889, () => {
+  console.log('Server listening at port 7889.');
+});
