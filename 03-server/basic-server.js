5fc7e56f828880573f225b7e756fb12c8c726b7d
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-01-01 11:18:57 +0800

    Support multiple route and methods

--- 03-server/basic-server.js
+++ 03-server/basic-server.js
@@ -1,7 +1,28 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end(`Hello World`);
+  if (req.url === '/') {
+    return res.end('Hello World');
+  }
+
+  if (req.url === '/end') {
+    if (req.method === 'POST') {
+      // TODO: get post data
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      return res.end(
+        JSON.stringify({
+          message: 'Bye',
+        })
+      );
+    } else {
+      return res.end('Bye!');
+    }
+  }
+
+  res.writeHead(404);
+  res.end(`Not Found`);
 });
 
 server.listen(7889, () => {
