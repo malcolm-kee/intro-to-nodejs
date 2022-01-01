b129b612a6828ca8ad11f65d975636be19e4cc24
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-01-01 15:30:42 +0800

    Respond different result based on accept header

--- 03-server/my-server.js
+++ 03-server/my-server.js
@@ -1,6 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  const accept = req.headers.accept;
+
+  if (accept === 'application/json') {
+    return res.end(
+      JSON.stringify({
+        number: Math.random(),
+      })
+    );
+  }
+
   res.end(Math.random().toString());
 });
 
