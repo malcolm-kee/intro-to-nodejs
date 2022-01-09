d4af14648d29006140d018e2c7f7b7a70c9f07ba
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-01-09 22:46:11 +0800

    change movie service to use database

--- 04-database/movie/movie.service.js
+++ 04-database/movie/movie.service.js
@@ -1,39 +1,52 @@
 const path = require('path');
 const fs = require('fs/promises');
-const { v4: uuid } = require('uuid');
 const { existsSync } = require('fs');
+const { query } = require('../db');
 
 const dataFile = path.resolve(__dirname, 'movies.json');
 
 const getMovies = () => {
-  if (existsSync(dataFile)) {
-    return fs.readFile(dataFile, 'utf-8').then((raw) => JSON.parse(raw));
-  }
-  return Promise.resolve([]);
+  return new Promise((fulfill, reject) => {
+    query('SELECT * FROM movies', [], (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      fulfill(result.rows);
+    });
+  });
 };
 
 exports.getMovies = getMovies;
 
 const addMovie = ({ title, language }) => {
-  const id = uuid();
-
-  const movie = {
-    id,
-    title,
-    language,
-  };
-
-  return getMovies()
-    .then((movies) =>
-      fs.writeFile(dataFile, JSON.stringify(movies.concat(movie)), 'utf-8')
-    )
-    .then(() => movie);
+  return new Promise((fulfill, reject) => {
+    query(
+      'INSERT INTO movies (title, language) VALUES ($1, $2) RETURNING *',
+      [title, language],
+      (err, result) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        fulfill(result.rows[0]);
+      }
+    );
+  });
 };
 
 exports.addMovie = addMovie;
 
 const getOneMovie = (id) => {
-  return getMovies().then((movies) => movies.find((movie) => movie.id === id));
+  return new Promise((fulfill, reject) => {
+    query('SELECT * FROM movies WHERE id = $1', [id], (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      fulfill(result.rows[0]);
+    });
+  });
 };
 
 exports.getOneMovie = getOneMovie;
